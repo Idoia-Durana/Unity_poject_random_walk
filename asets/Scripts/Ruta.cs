d8e71b0b4f7c23fd97201a7be68d35d4cb5dedde
Idoia Durana <idoia.durana@gmail.com>
2024-08-14 12:40:38 +0200

    Ruta.cs

--- asets/Scripts/Ruta.cs
+++ asets/Scripts/Ruta.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Ruta : MonoBehaviour
 {
-    public GameObject prefabRuta;// Un GameObject que servirá como la plantilla para crear partes de la ruta
-    public Vector3 ultimaPosicion; // La última posición en la que se creó una parte de la ruta
-    public float diferencia = 0.7f; //La diferencia en las coordenadas x y z entre las partes de la ruta
-    private int cuentaRuta = 0; //Un contador que lleva la cuenta del número de partes de la ruta creadas
-    //IniciarConstruccion Invoca repetidamente el método CrearNuevaParteRuta() cada 5 seg, comenzando después de 1 seg
+    public GameObject prefabRuta;
+    public Vector3 ultimaPosicion; 
+    public float diferencia = 0.7f; 
+    private int cuentaRuta = 0; 
+
+    
     public void IniciarConstruccion()
     {
         InvokeRepeating("CrearNuevaParteRuta", 1f, 0.5f);
@@ -18,38 +19,33 @@ public class Ruta : MonoBehaviour
         print("Crear parte ruta nueva");
 
         Vector3 nuevaPosicion = Vector3.zero;
-        float opcion = Random.Range(0, 100);//Genera una posición para la nueva parte de la ruta de manera aleatoria
+        float opcion = Random.Range(0, 100);
 
         if (opcion < 50)
         {
             nuevaPosicion = new Vector3(ultimaPosicion.x +
                 diferencia, ultimaPosicion.y, ultimaPosicion.z 
-                + diferencia);//Si un número aleatorio es menor que 50,
-                              //la nueva posición se crea desplazando la última posición en
-                              //las coordenadas x y z positivas
+                + diferencia);
         }
         else
         {
             nuevaPosicion = new Vector3(ultimaPosicion.x - 
                 diferencia, ultimaPosicion.y, ultimaPosicion.z 
-                + diferencia);//Si el número aleatorio es mayor o igual a 50,
-                              //la nueva posición se crea desplazando la última posición en
-                              //las coordenadas x negativas y z positivas
+                + diferencia);
 
         }
-        GameObject g = Instantiate(prefabRuta, nuevaPosicion, Quaternion.Euler(0, 45, 0)); //Instancia un nuevo objeto prefabRuta
-                                                                                           //en la nueva posición con una rotación
-                                                                                           //de 45 grados alrededor del eje Y
+        GameObject g = Instantiate(prefabRuta, nuevaPosicion, Quaternion.Euler(0, 45, 0)); 
 
-        ultimaPosicion = g.transform.position;//Actualiza la variable ultimaPosicion con la posición del nuevo objeto.
+        ultimaPosicion = g.transform.position;
 
         cuentaRuta++; //Incrementa el contador cuentaRuta.
-        if (cuentaRuta % 5 == 0) //Si el contador es divisible por 5, activa el primer hijo del objeto creado
+        if (cuentaRuta % 5 == 0) 
         {
             g.transform.GetChild(0).gameObject.SetActive(true);
         }
     }
-    //Verifica si se presionó la tecla Espacio y, en ese caso, llama al método CrearNuevaParteRuta()
+
+   
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
