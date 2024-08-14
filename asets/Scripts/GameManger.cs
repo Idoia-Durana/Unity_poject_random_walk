55dea705824e9639b97079666f1f75bc62c6a74e
Idoia Durana <idoia.durana@gmail.com>
2024-08-14 12:42:32 +0200

    GameManger.cs

--- asets/Scripts/GameManger.cs
+++ asets/Scripts/GameManger.cs
@@ -7,48 +7,42 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public bool juegoIniciado;// variable que marca si el juego está iniciado o no
-    public int puntuaje; //Esta variable va a almacenar un valor en el método AumentarPuntuaje autoincrementandose (puntuaje++)
-    public Text textoPuntuaje; // variable que mostrará el puntaje actual en la interfaz del usuario.
-    public Text puntuajeMaximoTexto;//variable de texto que mostrará el puntaje máximo alcanzado en la interfaz del usuario.
+    public bool juegoIniciado;
+    public int puntuaje; 
+    public Text textoPuntuaje; 
+    public Text puntuajeMaximoTexto;
 
+    
     private void Awake()//Awake Se utiliza para inicializar variables antes de que comience la ejecución del juego.
     {
-        puntuajeMaximoTexto.text = "Mejor: " + ObtenerPuntuajeMaximo().ToString(); //Establece el texto del puntaje máximo
-                                                                                   //(puntuajeMaximoTexto) con el valor recuperado
-                                                                                   //de ObtenerPuntuajeMaximo().
+        puntuajeMaximoTexto.text = "Mejor: " + ObtenerPuntuajeMaximo().ToString(); 
     }
+
 
     public void IniciarJuego()
     {
-        juegoIniciado = true;//Establece juegoIniciado como verdadero.
-        FindObjectOfType<Ruta>().IniciarConstruccion();//llama al método IniciarConstruccion() del objeto Ruta,
-                                                       //que es el responsable de iniciar la construcción de la ruta en el juego.
+        juegoIniciado = true;
+        FindObjectOfType<Ruta>().IniciarConstruccion();
     }
 
     public void FinalizarJuego()
     {
-        //Este método es llamado por la clase ControlPersonaje(Update)
-        //Dentro de el método FinalizarJuego llamamos a un sceneManager.LoadScene que se u utiliza para cargar el numero de
-        //índice de una escena,al tener una sola escena usamos el 0, se carga en los built setting y se indexa con ese numero
         SceneManager.LoadScene(0);
     }
 
     public void AumentarPuntuaje()
     {
-        puntuaje++;//Incrementa el puntaje (puntuaje++).
+        puntuaje++;
         textoPuntuaje.text = puntuaje.ToString();
-        //Actualiza el texto del puntaje(textoPuntuaje.text) con el valor actualizado del puntaje.
+        
 
-        if (puntuaje > ObtenerPuntuajeMaximo())//Verifica si el puntaje actual es mayor que el puntaje máximo almacenado.Si es así:
+        if (puntuaje > ObtenerPuntuajeMaximo())
         {    
-            PlayerPrefs.SetInt("PuntuajeMaximo",puntuaje);//Almacena el nuevo puntaje máximo en PlayerPrefs con la clave "PuntuajeMaximo".
-            puntuajeMaximoTexto.text = "Mejor: " + puntuaje.ToString();//Actualiza el texto del puntaje máximo (puntuajeMaximoTexto.text)
-                                                                       //con el nuevo valor del puntaje máximo.
+            PlayerPrefs.SetInt("PuntuajeMaximo",puntuaje);
+            puntuajeMaximoTexto.text = "Mejor: " + puntuaje.ToString();
         }
     }
 
-    //El método ObtenerPuntuajeMaximo Recupera el puntaje máximo almacenado en PlayerPrefs con la clave "PuntuajeMaximo" y lo devuelve.
     public int ObtenerPuntuajeMaximo()
     {
         int i = PlayerPrefs.GetInt("PuntuajeMaximo");
