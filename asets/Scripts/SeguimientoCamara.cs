023e116bfe2550faf37961b1aa9ed371e31b9aa0
Idoia Durana <idoia.durana@gmail.com>
2024-08-14 12:39:29 +0200

    Update SeguimientoCamara.cs

--- asets/Scripts/SeguimientoCamara.cs
+++ asets/Scripts/SeguimientoCamara.cs
@@ -5,12 +5,9 @@ using UnityEngine;
 public class SeguimientoCamara : MonoBehaviour
 {
     public Transform objetivo; 
-    private Vector3 diferencia; //variable tridimensional que almacenará la diferencia entre la posición de
-                                //la cámara y la posición del objetivo
+    private Vector3 diferencia; 
 
-    //Metodo Awake Se utiliza para inicializar variables u objetos antes de que comience la ejecución del juego.
-    //Calcula la diferencia entre la posición inicial de la cámara y la posición del objetivo.Esta diferencia
-    //se calcula restando la posición del objetivo(objetivo.position) de la posición de la cámara(transform.position).
+    
     void Awake()
     {
         //diferencia = posición cámara – posición jugador
@@ -18,9 +15,6 @@ public class SeguimientoCamara : MonoBehaviour
     }    
     private void LateUpdate()
     {
-        //la position de la camara va a ser igual a
-        //la posición del personaje + diferencia,
-        //si no añadiésemos la diferencia la cámara se encontraría dentro del personaje
         transform.position = objetivo.position + diferencia;
     }
 }
